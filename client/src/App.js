835c703599a786c827cd4db2248063451ac4a14f
Wayne <wayne.c.griffiths@gmail.com>
2020-07-14 17:20:15 +0800

    UPDATE package.json to lastest version of @wannabewayno/reactor and react-router-dom. ADD: Saved, Search and NotFound pages/react components, Added these to App.js, renders fine

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Dropdown, Number, InlineContainer, Container } from '@wannabewayno/reactor';
+import Saved from './pages/Saved';
+import Search from './pages/Search';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path={["/", "/search"]}>
+          <Search/>
+        </Route>
+        <Route exact path="/saved">
+          <Saved/>
+        </Route>
+        <Route>
+          <NotFound/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
