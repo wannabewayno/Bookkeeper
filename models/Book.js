4d83dcd1ed45955563568ad5b42fd57629c57a49
Wayne <wayne.c.griffiths@gmail.com>
2020-07-15 14:13:27 +0800

    ADD Book schema in Models, ADD mongoConnection.js under config, ADD placeholder for Controllers

---
+++ models/Book.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Schema
+// ==============================================================================
+
+const bookSchema = new Schema({
+
+    title: String,
+    authors: [ String ],
+    description: String,
+    image: String,
+    link: String,
+
+});
+
+const Book = mongoose.model('Book', bookSchema );
+module.exports = Book; 
