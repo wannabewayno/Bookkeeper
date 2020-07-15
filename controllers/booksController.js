34fc6f72779d59c80465a434b2b4eb39df8a4628
Wayne <wayne.c.griffiths@gmail.com>
2020-07-15 15:03:42 +0800

    ADD methods to booksControllers.js

---
+++ controllers/booksController.js
@@ -0,0 +1,27 @@
+// Use the Book schema in booksController
+const { Book } = require('../models');
+
+// Methods for the booksController
+module.exports = {
+    findAll(req,res) {
+        Book.find()
+        .then(books => res.json(books))
+        .catch(error => res.status(422).json(error));
+    },
+    findById(req,res) { 
+        Book.findById(req.params.id)
+        .then(book => res.json(book))
+        .catch(error => res.status(422).json(error));
+    },
+    save(req,res) { 
+        Book.create(req.body)
+        .then(response => res.json(response))
+        .catch(error => res.status(422).json(error));
+    },
+    delete(req,res) {
+        Book.findById({ _id: req.params.id })
+        .then(bookToDelete => bookToDelete.remove())
+        .then(deletedBook => res.json(deletedBook))
+        .catch(error => res.status(422).json(error));
+    },
+}
