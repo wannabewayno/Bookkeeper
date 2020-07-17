81c0bacd17e606a88d44bb0bacf992fce39bc53f
Wayne <wayne.c.griffiths@gmail.com>
2020-07-17 20:56:45 +0800

    deploy

--- client/src/pages/Search/index.jsx
+++ client/src/pages/Search/index.jsx
@@ -2,15 +2,17 @@ import React, { useState } from 'react';
 import { Container, FormContainer, SearchBar, ResultContainer, InlineContainer, Arctext } from '@wannabewayno/reactor';
 import './style.css';
 import { createBook } from '../../utils/API';
+import { searchBooks } from '../../utils/API/googleBooks';
 
 export default function Search(){
 
     // const [ bookQuery, setBookQuery ] = useState({});
     // const [ bookData, setBooKData ] = useState([]);
 
-    function handleFormSubmit(formData){
+    async function handleFormSubmit(formData){
         console.log('FORM DATA:',formData);
-        createBook(formData);
+        const books = searchBooks(formData);
+        console.log(books);
     } 
 
     return (
@@ -18,22 +20,23 @@ export default function Search(){
             <div style={{backgroundColor:'rgb(26,116,88)', width:'fit-content', margin:'auto',padding:'0.5rem',borderRadius:'5px'}}>Feeling bookish?</div>
 
             <FormContainer onSubmit={handleFormSubmit}>
-                <InlineContainer gap='1rem' minWidth='150px'>
-                	<SearchBar name={{display:'',id:'title',toDisplay:true}} backgroundColor='rgb(26,116,88)'/>
-                	<SearchBar name={{display:'',id:'authors'}} backgroundColor='rgb(26,116,88)'/>
-                	<SearchBar name={{display:'',id:'description'}} backgroundColor='rgb(26,116,88)'/>
+                <InlineContainer gap='1rem' minWidth='200px'>
+                    <InlineContainer gap='1rem' minWidth='200px'>
+                        <SearchBar name={{display:'',id:'search',toDisplay:true}} backgroundColor='rgb(26,116,88)'/>
+                        <SearchBar name={{display:'',id:'title',toDisplay:true}} backgroundColor='rgb(26,116,88)'/>
+                    </InlineContainer>
+                    <InlineContainer gap='1rem' minWidth='200px'>
+                        <SearchBar name={{display:'',id:'author',toDisplay:true}} backgroundColor='rgb(26,116,88)'/>
+                        <SearchBar name={{display:'',id:'subject',toDisplay:true}} backgroundColor='rgb(26,116,88)'/>
+                    </InlineContainer>
                 </InlineContainer>
                 <button type='submit'>Submit</button>
             </FormContainer>
 
-            <Arctext>
-                this is Arctext
-            </Arctext>
-
             <ResultContainer results={[]}>
                 <div></div>
             </ResultContainer>
-
+           
         </Container>
     )
 }
---
+++ client/src/utils/API/googleBooks.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+export async function searchBooks(formData){
+    console.log('SearchBooks:',formData);
+    const API_KEY = `&key=AIzaSyCNi4md3S-WZAswlzNHKqn-yiwD8Ii9Wek`;
+    const baseQuery = 'https://www.googleapis.com/books/v1/volumes?q='
+    const general = '';
+    const inAuthor = `inauthor:${''}`;
+    const inTitle = `intitle:${''}`;
+    const subject = `subject${''}`;
+
+    const query = 'https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes'
+
+    try {
+        return await axios(query+API_KEY,{
+            credentials:'include',
+            mode:'cors',
+            headers:{
+                'Content-type':'application/json'
+            }
+        });
+    } catch(error) {
+        console.error('An error occured searching for books:',error);
+    }
+}
